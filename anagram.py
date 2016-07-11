6967d2012fddb3453ee349c7b8c6f205a661d183
Jay Chan <crookedjustice@hotmail.com>
2016-07-11 02:43:01 -0700

    Initial commit

---
+++ anagram.py
@@ -0,0 +1,108 @@
+from itertools import combinations
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PRIMES = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101
+]
+LETTERS_TO_PRIMES = dict(zip(LETTERS, PRIMES))
+
+class Anagram(object):
+
+    def __init__(self):
+        self.data = {}
+        self.tiered = [{} for i in xrange(16)]
+
+        fh = open('twl.txt', 'r')
+        for line in fh:
+            line = line.strip()
+            hx = self.hash(line)
+            self.data.setdefault(hx, []).append(line)
+            self.tiered[len(line)].setdefault(hx, []).append(line)
+
+    def hash(self, letters):
+        r = 1
+        for c in letters:
+            r *= LETTERS_TO_PRIMES[c]
+        return r
+
+    def lookup(self, letters):
+        return self.data.get(self.hash(letters), [])
+
+    def snatch(self, words):
+        res = set()
+        letters = [w for w in words if len(w) == 1]
+        words = [w for w in words if len(w) > 1]
+
+        letter_hashes = []
+        for j in xrange(min(len(letters) + 1, 15)):
+            entries = []
+            for l in combinations(letters, j):
+                entries.append((l, self.hash(''.join(l))))
+            letter_hashes.append(entries)
+
+        for i in xrange(min(len(words) + 1, 4)):
+            for w in combinations(words, i):
+                combo = ''.join(w)
+                if len(combo) > 15:
+                    continue
+                words_hx = self.hash(combo)
+
+                for entries in letter_hashes[max(0, 2 - len(w)):]:
+                    for l, letter_hx in entries:
+                        hx = words_hx * letter_hx
+                        if hx not in self.data:
+                            continue
+                        for built in self.data[hx]:
+                            res.add((tuple(sorted(w) + sorted(l)), built))
+
+        res = list(res)
+        res.sort(key=lambda r: (len(r[1]), r[0]))
+        return res
+
+    def extend(self, word):
+        res = set()
+        ix = self.hash(word)
+        for i in xrange(len(word), 15):
+            for hx, builts in self.tiered[i].iteritems():
+                if hx >= ix and hx % ix == 0:
+                    for built in builts:
+                        sub = self.subtract(built, word)
+                        res.add((tuple([word] + sorted(sub)), built))
+
+        res = list(res)
+        res.sort(key=lambda r: (len(r[1]), r[0]))
+        return res
+
+    def subtract(self, a, b):
+        a = list(a)
+        for c in b:
+            a.remove(c)
+        return tuple(a)
+
+if __name__ == '__main__':
+    import time
+    a = time.time()
+    anagram = Anagram()
+    print time.time() - a
+
+    a = time.time()
+    res = anagram.snatch("""
+        adjunct professor anomally detection
+        machine learning splitting heading
+        proclaim announce jinx hybrid
+        dexter corpse anneal index
+        marbling dietary genious
+        dubious apple tides marches
+        petered daffodill hungry
+        q r g k i o
+    """.upper().strip().split())
+    for combo, built in res:
+        print ' + '.join(combo), '=', built
+    print time.time() - a
+
+    a = time.time()
+    res = anagram.extend('TEACH')
+    for combo, built in res:
+        print ' + '.join(combo), '=', built
+    print time.time() - a
---
+++ webapp.py
@@ -0,0 +1,105 @@
+import os
+import web
+import re
+import sys
+import time
+
+os.chdir(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__))
+
+from anagram import Anagram
+
+anagram = Anagram()
+
+urls = (
+    '/?', 'index',
+)
+app = web.application(urls, globals())
+
+template = """
+    <html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <style>
+            textarea {
+                min-height: 150px !important;
+                resize: vertical !important;
+            }
+            #small {
+                padding-top: 50px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="container-fluid"><div class="row">
+    <div class="col-sm-12"><h3>Anagram/Snatch Solver</h3></div>
+    <div class="col-sm-12">
+        <form id="qform" action="" method="post">
+            <div class="form-group">
+            <textarea class="form-control"
+                name="q" autofocus>%s</textarea>
+            </div>
+            <div class="form-group">
+            <button class="btn btn-default" type="submit">Solve!</button>
+            </div>
+        </form>
+    </div>
+
+    </div>
+    <div class="row">%s</div>
+    <div class="row" id="small"><div class="col-xs-12">
+        <small>Generated in %0.4f secs.<small>
+    </div></div>
+    </div>
+    </body></html>
+"""
+
+def col(s, klass=''):
+    return '<div class="col-xs-12 col-sm-6 col-lg-3 %s">%s</div>\n' % (klass, s)
+
+def fmt(combo, built):
+    ret = ''
+    for c in combo:
+        words = ' + '.join(w for w in combo if len(w) > 1)
+        letters = ' '.join(w for w in combo if len(w) == 1)
+    return ' + '.join(filter(None, [words, letters])) + ' = ' + built
+
+class index(object):
+    def GET(self):
+        start_time = time.time()
+        form = web.input(q='')
+        form_q = re.sub(r'[^a-zA-Z\n]', ' ', form.q)
+        form_q = re.sub(r'[ \t]+', ' ', form_q)
+        form_q = re.sub(r'\s*\n\s*', '\n', form_q)
+        form_q = form_q.upper().strip()
+
+        data = form_q.split()
+        if not data or len(''.join(data)) < 4:
+            result_str = []
+        else:
+            if len(data) == 1:
+                result = anagram.extend(data[0])
+            else:
+                result = anagram.snatch(data)
+
+            if not result:
+                result_str = [col('No Solution!')]
+            else:
+                result_str = []
+                for combo, built in result:
+                    result_str += [col(fmt(combo, built))]
+
+        for word in data:
+            if len(word) <= 1:
+                continue
+            if word not in anagram.lookup(word):
+                result_str.insert(0, col(word + ' is not a word', klass='bg-danger'))
+
+        end_time = time.time()
+
+        return template % (form_q, ''.join(result_str), end_time - start_time)
+
+    POST = GET
+
+application = app.wsgifunc()
