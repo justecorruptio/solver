0cff79db52d93e756b04c58bd012479b760d3171
Jay Chan <crookedjustice@hotmail.com>
2026-04-14 07:49:49 +0000

    remove some more prototype overrides

--- jx/solver.js
+++ jx/solver.js
@@ -1,6 +1,4 @@
-Object.prototype.keys = function(){return Object.keys(this)};
 String.prototype.sort = function(){return [...this].sort().join('')};
-String.prototype.upper = function(){return this.toUpperCase()};
 $ = x => document.querySelector(x);
 $$ = x => document.querySelectorAll(x);
 LETTERS = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
@@ -10,13 +8,13 @@ fetchFile = async (fn, cb) => (await (await fetch(fn)).text()).match(/[^\n]+/g).
 
 Promise.all([
     fetchFile('nwl2023.txt', word => {
-        word = word.upper();
+        word = word.toUpperCase();
         ALL[word] = true;
         (GRAMS[word.sort()] ||= []).push(word);
     }),
     fetchFile('def.txt', line => {
         let [, words, def, pos] = line.match(/(.+)\t([ A-Z]+([a-z]+).+)/);
-        words.upper().split(' ').forEach(word => {
+        words.toUpperCase().split(' ').forEach(word => {
             (DEFS[word] ||= []).push(def);
             (POS[word] ||= {})[pos] = 1;
         });
@@ -33,14 +31,13 @@ ulu = (pattern) => {
     return (word) => check_len(word.length) && word.sort().match(regex) && [word];
 }
 
-sm = (cls, text) => `<small class="annotation ${cls}">${text}</small>`;
-
 cell = (str, cls, word) => `<cell class="${cls || ''}"
     ${word ? `data-word="${word}" data-hash="${word.sort()}"` : ''}
     onclick="handleClickCell(event)">${str}</cell>`;
 
 annotate = (word) => {
-    let hook = (a, b) => LETTERS.filter(l => ALL[a + l + b]).join(''),
+    let sm = (cls, text) => `<small class="annotation ${cls}">${text}</small>`,
+        hook = (a, b) => LETTERS.filter(l => ALL[a + l + b]).join(''),
         hidden = $('#checkAnnotate').checked ? '' : 'hidden',
         rank = (RANKS[word.sort()] || 99999) / 20000,
         dot = w => ALL[w] ? '●' : '';
@@ -48,18 +45,18 @@ annotate = (word) => {
         ${sm('left', hook('', word) + dot(word.slice(1)))}
         ${sm('super left', '★'.repeat(rank))} ${word}
         ${sm('right', dot(word.slice(0,-1)) + hook(word, ''))}
-        ${sm('super right', (POS[word] || {}).keys().join('&nbsp;'))}
+        ${sm('super right', Object.keys(POS[word] || {}).join('&nbsp;'))}
     </div>`;
 }
 
 handleFind = (type) => {
-    var input = $('#input').value = $('#input').value.trim().upper();
+    var input = $('#input').value = $('#input').value.trim().toUpperCase();
     if (!input) return;
     var isRegexMode = $('#checkRegex').checked,
         [regex, ...clauses] = input.match(/([^ :]+)/g),
         wrappedRegex = `^(?:${regex.replace(/@/g,'(.+)')})$`,
         sortKey = type == 'alpha' ? w => w.sort() : w => w;
-    var res = ALL.keys().map(
+    var res = Object.keys(ALL).map(
         isRegexMode ? word => word.match(wrappedRegex) : ulu(regex)
     ).filter(matches => matches && clauses.every(clause => {
         var [, op, len] = clause.match(/^([<>=])(\d+)$/) || [];
@@ -88,7 +85,7 @@ getCell = (el, success) => {
 }
 
 handleInput = ({keyCode, shiftKey}) => {
-    var input = $('#input').value.upper().trim();
+    var input = $('#input').value.toUpperCase().trim();
     if (!input || keyCode != 13) return;
     if (shiftKey) return handleFind();
     var el = $(`cell[data-word="${input}"]`);
@@ -103,11 +100,9 @@ handleReveal = () => $$('cell.hidden').forEach(x => x.classList.replace('hidden'
 handleClickCell = ({target, target: {dataset: {word}}}) => {
     if (target.matches('.err')) return target.remove();
     if (target.matches('.hidden')) return getCell(target, false);
-    if (word) {
-        $('#def').innerText = (DEFS[word]||[]).join('\n') || 'No definition';
-        $('#def').style.display = 'block';
-    }
+    if (!word) return;
+    $('#def').innerText = (DEFS[word]||[]).join('\n') || 'No definition';
+    $('#def').style.display = 'block';
 }
 
-visualViewport.onresize = ({target: {height}}) =>
-    document.documentElement.style.setProperty('--vh', height + 'px');
+visualViewport.onresize = ({target: {height}}) => $('html').style.setProperty('--vh', height + 'px');
